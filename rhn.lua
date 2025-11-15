af39521eeed5840eceff6f8f2a75c826dc9eebfd
rhnmesomorf-creator <rhnmesomorf@gmail.com>
2025-11-15 14:51:50 +0800

    Create rhn.lua
    Signed-off-by: rhnmesomorf-creator <rhnmesomorf@gmail.com>

---
+++ rhn.lua
@@ -0,0 +1,10 @@
+local games = {}
+
+local currentID =
+local scriptURL =
+
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
+end
